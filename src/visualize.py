87a08edab24170debd623c79493e1853fcfcb975
Phuong Nguyen <phuongn008@gmail.com>
2022-03-13 01:22:32 -0800

    Weighted Random Sampler + Visualization

--- src/visualize.py
+++ src/visualize.py
@@ -1,3 +1,6 @@
+import seaborn as sns
+import torch
+
 from collections import Counter
 import matplotlib.pyplot as plt
 
@@ -14,5 +17,16 @@ def view_data_distribution(dataset, name: str):
 
     plt.savefig(f'visualizations/{name}.png')
 
-def confusion_matrix():
-    raise NotImplementedError
+def save_confusion_matrix(cf):
+    
+    normalized_cf = cf / torch.sum(cf)
+
+    cf_heatmap = sns.heatmap(normalized_cf, annot=True, fmt='.1%', cmap="Blues")
+    
+    cf_heatmap.set_title("GTSRB Confusion Matrix with labels (as original integers)")
+    cf_heatmap.set_xlabel('\nPredicted Values')
+    cf_heatmap.set_ylabel('Actual Values')
+
+    sns.set(rc={'figure.figsize': (35, 25)})
+
+    plt.savefig('visualizations/confusion_matrix_GTSRB.png', dpi = 300)
